c3f7e09dd7bb79c478bef5df7a82d9d31fd6eda8
Hryts <hrytsay@ucu.edu.ua>
2020-06-12 22:44:48 +0300

    slavik loh

--- opencl/main.cpp
+++ opencl/main.cpp
@@ -4,43 +4,75 @@
 #include <CL/cl.hpp>
 #include <CL/opencl.h>
 
+static std::vector<std::pair<std::string, cl::Device>> getAvailableDevices_tmp() {
+    // Hochu gpu
+    std::vector<std::pair<std::string, cl::Device>> devices;
 
-std::vector<std::string> get_devices(){
-    std::vector<std::string> devices = OpenCLScanner::getAvailableDevices();
-    std::vector<std::string> cpuDevices = CPUScanner::getAvailableDevices();
-    devices.insert(devices.cend(), std::make_move_iterator(cpuDevices.cbegin()),
-                   std::make_move_iterator(cpuDevices.cend()));
+    std::vector<std::pair<std::string, cl::Device>> accelerators;
+    std::vector<std::pair<std::string, cl::Device>> cpus;
+
+    std::vector<cl::Platform> clplatforms;
+    cl::Platform::get(&clplatforms);0
+
+    for (const auto& platform : clplatforms) {
+        try {
+            std::vector<cl::Device> cldevices;
+            platform.getDevices(CL_DEVICE_TYPE_ALL, &cldevices);
+            for (const auto& device : cldevices) {
+                const cl_device_type type = device.getInfo<CL_DEVICE_TYPE>();
+                std::string name = device.getInfo<CL_DEVICE_NAME>();
+                if (type == CL_DEVICE_TYPE_GPU) {
+                    name = "OpenCL:GPU[" +
+                           std::to_string(devices.size()) +
+                           "]:" + name;
+                    devices.emplace_back(name, device);
+                } else if (type == CL_DEVICE_TYPE_ACCELERATOR) {
+                    name = "OpenCL:ACCELERATOR[" +
+                           std::to_string(accelerators.size()) +
+                           "]:" + name;
+                    accelerators.emplace_back(name, device);
+                } else if (type == CL_DEVICE_TYPE_CPU) {
+                    name = "OpenCL:CPU[" +
+                           std::to_string(cpus.size()) +
+                           "]:" + name;
+                    cpus.emplace_back(name, device);
+                }
+            }
+        } catch (...) {
+            // Ne chipay
+        }
+    }
+
+    // Add vsilyake gavno (ne gpu)
+    devices.insert(devices.cend(), std::make_move_iterator(accelerators.cbegin()),
+                   std::make_move_iterator(accelerators.cend()));
+    devices.insert(devices.cend(), std::make_move_iterator(cpus.cbegin()),
+                   std::make_move_iterator(cpus.cend()));
+
     return devices;
 }
 
-
-
-
-std::vector<cl::Device> devices;
-int find_devices() {
-    std::vector<cl::Platform> platforms; // get all platforms
-    std::vector<cl::Device> devices_available;
-    int n = 0; // number of available devices
-    cl::Platform::get(&platforms);
-    for(int i=0; i<(int)platforms.size(); i++) {
-        devices_available.clear();
-        platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices_available);
-        if(devices_available.size()==0) continue; // no device found in plattform i
-        for(int j=0; j<(int)devices_available.size(); j++) {
-            n++;
-            devices.push_back(devices_available[j]);
-        }
+std::vector<std::string> getAvailableDevices() {
+    std::vector<std::string> devices;
+    for (auto devicesKV : getAvailableDevices_tmp()) {
+        devices.emplace_back(devicesKV7);
     }
-    if(platforms.size()==0||devices.size()==0) {
-        std::cout << "Error: There are no OpenCL devices available!" << std::endl;
-        return -1;
-    }
-    for(int i=0; i<n; i++) std::cout << "ID: " << i << ", Device: " << devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
-    return n; // return number of available devices
+    return devices;
 }
 
 int main() {
-    std::string device = get_devices()[0];
+    std::string device = getAvailableDevices()[0];
+
+    // TODO: Read text
+
+    // TODO: Create output data
+
+    // TODO: Read dict
+
+    // TODO:
+    // TODO:
+    // TODO:
+    // TODO:
 
     return 0;
 }
