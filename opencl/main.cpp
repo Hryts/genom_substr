4ad079f50c00a52cbc9561c43240739b93f796ac
Hryts <hrytsay@ucu.edu.ua>
2020-06-14 12:15:34 +0300

    initialized opencl host

--- opencl/main.cpp
+++ opencl/main.cpp
@@ -8,6 +8,7 @@
     #include <CL/opencl.h>
 #endif
 
+constexpr auto PROGRAM_PATH = "."; // Path to source file
 
 static std::vector<std::pair<std::string, cl::Device>> getAvailableDevices_tmp() {
     // Hochu gpu
@@ -57,27 +58,37 @@ static std::vector<std::pair<std::string, cl::Device>> getAvailableDevices_tmp()
     return devices;
 }
 
-std::vector<std::string> getAvailableDevices() {
+std::vector<std::string> getNeededDevices() {
     std::vector<std::string> devices;
-    for (auto devicesKV : getAvailableDevices_tmp()) {
-        devices.emplace_back(devicesKV.first);
+    for (auto device : getAvailableDevices_tmp()) {
+        devices.emplace_back(device.first);
     }
     return devices;
 }
 
 int main() {
-    std::string device = getAvailableDevices()[0];
+    cl::Device device;
+    cl::Context context;
+    cl::Kernel pfacKernel;
+
+    device = getAvailableDevices_tmp()[0].second;
+    context = cl::Context(device);
+
+
+    // TODO: Роздуплитись
+    auto sourceBytes = readFile(PROGRAM_PATH);
+
+    const cl::Program::Sources sources = {{sourceBytes.data(), sourceBytes.size()}};
+    cl::Program program = cl::Program(context, sources);
+
+    pfacKernel = cl::Kernel(program, "our_pfac");
+
 
     // TODO: Read text
 
     // TODO: Create output data
 
     // TODO: Read dict
-
-    // TODO:
-    // TODO:
-    // TODO:
-    // TODO:
 
     return 0;
 }
